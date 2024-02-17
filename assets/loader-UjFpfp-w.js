a905532671d512652a2ac57b7ef572537541bd7c
GeoStrong <GeoStrong@users.noreply.github.com>
2024-02-17 21:31:11 +0000

    deploy: 6800fc702da5ae36a8f5047947bdc95a0f205a1e

---
+++ assets/loader-UjFpfp-w.js
@@ -0,0 +1,1 @@
+import{getData as r,productsFirstUrl as o,productsSecondUrl as t}from"./config-gMOlDiSH.js";import{ac as e}from"./index-kJarJC0y.js";const a=async()=>e({products:r(`${o}products`),categories:r(`${o}categories`),discountProducts:r(`${t}?limit=5`),moreCategories:r(`${t}/categories`)});export{a as loader};

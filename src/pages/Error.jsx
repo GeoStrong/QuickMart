6800fc702da5ae36a8f5047947bdc95a0f205a1e
geotrong <dzobavageorgij@gmail.com>
2024-02-18 01:29:55 +0400

    Update Error page layout

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -12,7 +12,7 @@ const Error = () => {
     <>
       <Container className="mt-3">
         <HeaderNavigation page="Error" />
-        <div className="text-center mt-3 fw-bold fs-3">
+        <div className="text-center pt-6 mt-3 fw-bold fs-3">
           <h1>{error.data ? error.data : error.message}</h1>
           <p>{error.status ? error.status : 500}</p>
         </div>

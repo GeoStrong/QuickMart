f65bb2daec44373bb23df687d37df24b0be02892
geotrong <dzobavageorgij@gmail.com>
2024-02-19 23:03:25 +0400

    Refactor accountDataLoader function to use defer() for asynchronous data loading

--- src/utilities/accountDataLoader.jsx
+++ src/utilities/accountDataLoader.jsx
@@ -1,4 +1,8 @@
+import { defer } from 'react-router-dom';
 import { accountUrl, authToken, getData } from './config';
 
-export const accountDataLoader = async (id) =>
-  await getData(`${accountUrl}/user_${id}.json${authToken}`);
+export const accountDataLoader = async (id) => {
+  return defer({
+    accountData: getData(`${accountUrl}/user_${id}.json${authToken}`),
+  });
+};

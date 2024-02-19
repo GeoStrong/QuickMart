245006e4873ff72f81e608c1535f9003b95cde6b
geotrong <dzobavageorgij@gmail.com>
2024-02-20 00:47:53 +0400

    Add HeaderNavigation component to useCheckScreenSize hook

--- src/hooks/useCheckScreenSize.jsx
+++ src/hooks/useCheckScreenSize.jsx
@@ -3,13 +3,20 @@ import Footer from '../components/UI/GlobalUI/Footer';
 import Header from '@/components/Home/Header/Header';
 import SupportPanel from '@/components/Support/SupportPanel';
 import { useState } from 'react';
+import HeaderNavigation from '@/components/UI/GlobalUI/HeaderNavigation';
 
 const useCheckScreenSize = () => {
   const [footerFade, setFooterFade] = useState(true);
   const { innerWidth } = window;
   const isScreenMobile = innerWidth <= 992;
 
-  const renderHeader = !isScreenMobile && <Header />;
+  const renderHeader = (title, redirect) =>
+    isScreenMobile ? (
+      <HeaderNavigation page={title} whereTo={redirect} />
+    ) : (
+      <Header />
+    );
+
   const renderFooter = isScreenMobile && <Footer open={footerFade} />;
   const renderProfilePanel = !isScreenMobile && <ProfilePanel />;
   const renderSupportPanel = !isScreenMobile && <SupportPanel />;

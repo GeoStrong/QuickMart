b08e986637dcc59f6adfbcd22b4e4034cfa4dab0
geotrong <dzobavageorgij@gmail.com>
2024-02-29 01:21:21 +0400

    Add navigation and formik functionality to HeaderForm component

--- src/components/Home/Header/HeaderForm.jsx
+++ src/components/Home/Header/HeaderForm.jsx
@@ -5,75 +5,87 @@ import { Form, ListGroup } from 'react-bootstrap';
 import { useLockBodyScroll } from 'react-use';
 import Filter from '@/components/UI/GlobalUI/Filter';
 import './HeaderForm.scss';
+import { useNavigate } from 'react-router-dom';
+import useParentUrl from '@/hooks/useParentUrl';
 
-const HeaderForm = ({ onSubmit, searchState, searchRef }) => {
-  const { isScreenMobile } = useCheckScreenSize();
+const HeaderForm = ({ searchState, searchRef, separate = false }) => {
+  const navigate = useNavigate();
   const [isSearchFocused, setIsSearchFocused] = useState(false);
+  const { isScreenMobile } = useCheckScreenSize();
+  const { originPath, parentPath } = useParentUrl();
 
-  const getRecentSearch = (event) => {
-    searchRef.current.value = event.target.innerText;
-    searchRef.current.focus();
-  };
-
-  const blockScroll = isScreenMobile ? isSearchFocused || searchState : false;
+  const blockScroll = separate ? false : isScreenMobile ? searchState : false;
 
   useLockBodyScroll(blockScroll);
 
   const formik = useFormik({
     initialValues: { search: '' },
-    onSubmit,
+    onSubmit: (values) => {
+      navigate(`/${originPath}/search/?searchText=${values.search}`);
+      // setSearchParams(values);
+    },
   });
 
+  const getRecentSearch = (event) => {
+    searchRef.current.value = event.target.innerText;
+    formik.values.search = event.target.innerText;
+    searchRef.current.focus();
+  };
+
   return (
-    <Form
-      className={`stack__form pt-3 ${
-        isScreenMobile && 'stack__form-mobile w-100'
-      } 
+    <>
+      <Form
+        className={`stack__form ${separate ? 'p-3' : 'pt-3'} ${
+          isScreenMobile && 'stack__form-mobile w-100'
+        } 
         ${searchState && 'bg-white z-3'}
       `}
-      onSubmit={formik.handleSubmit}
-    >
-      <Form.Group
-        className={`stack__form-group position-relative mb-1 
-        ${searchState ? 'd-block' : 'd-none'}
-            `}
+        onSubmit={formik.handleSubmit}
+        style={{ height: `${!separate && isScreenMobile ? '100svh' : ''}` }}
       >
-        <Form.Control
-          ref={searchRef}
-          type="text"
-          id="search"
-          placeholder="Search"
-          className="stack__form-input py-3 rounded-3 ps-5 fw-normal"
-          onFocus={() => {
-            setIsSearchFocused(true);
-          }}
-          onBlur={() => {
-            setTimeout(() => {
-              setIsSearchFocused(false);
-            }, 100);
-          }}
-        />
-        <Filter />
-      </Form.Group>
-      {searchState && isSearchFocused && (
         <Form.Group
-          className="position-absolute w-100"
-          onClick={() => {
-            setIsSearchFocused(true);
-          }}
+          className={`stack__form-group position-relative mb-1 
+        ${searchState || separate ? 'd-block' : 'd-none'}
+            `}
         >
-          <Form.Text className="text-dark">RECENT SEARCH</Form.Text>
-          <ListGroup className="stack__list">
-            <ListGroup.Item
-              className="stack__item border-0 border-bottom small fw-medium"
-              onClick={getRecentSearch}
-            >
-              Cras justo odio
-            </ListGroup.Item>
-          </ListGroup>
+          <Form.Control
+            ref={searchRef}
+            type="text"
+            id="search"
+            placeholder="Search"
+            className="stack__form-input py-3 rounded-3 ps-5 fw-normal"
+            onFocus={() => {
+              setIsSearchFocused(true);
+            }}
+            onBlur={() => {
+              setTimeout(() => {
+                setIsSearchFocused(false);
+              }, 100);
+            }}
+            {...formik.getFieldProps('search')}
+          />
+          {parentPath === 'search' && <Filter />}
         </Form.Group>
-      )}
-    </Form>
+        {searchState && isSearchFocused && (
+          <Form.Group
+            className="position-absolute w-100"
+            onClick={() => {
+              setIsSearchFocused(true);
+            }}
+          >
+            <Form.Text className="text-dark">RECENT SEARCH</Form.Text>
+            <ListGroup className="stack__list">
+              <ListGroup.Item
+                className="stack__item border-0 border-bottom small fw-medium"
+                onClick={getRecentSearch}
+              >
+                Cras justo odio
+              </ListGroup.Item>
+            </ListGroup>
+          </Form.Group>
+        )}
+      </Form>
+    </>
   );
 };
 export default HeaderForm;

3abfa19ec754b1a9da58ab8ce399a8dff5a18d43
geotrong <dzobavageorgij@gmail.com>
2024-02-29 01:22:00 +0400

    Add badges to indicate non-empty cart and wishlist

--- src/components/UI/GlobalUI/Footer.jsx
+++ src/components/UI/GlobalUI/Footer.jsx
@@ -1,10 +1,22 @@
-import { Fade, Stack } from 'react-bootstrap';
+import { Badge, Fade, Stack } from 'react-bootstrap';
 import './Footer.scss';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useRouteLoaderData } from 'react-router-dom';
 import useCustomSvg from '../../../hooks/useCustomSvg';
 import useParentUrl from '../../../hooks/useParentUrl';
+import { useEffect, useState } from 'react';
+import { usePromise } from 'react-use';
 
 const Footer = ({ open }) => {
+  const loaderData = useRouteLoaderData('root');
+  const mounted = usePromise();
+
+  const [accountData, setAccountData] = useState();
+
+  const [emptyProduct, setEmptyProduct] = useState({
+    cartIsEmpty: true,
+    wishlistIsEmpty: true,
+  });
+
   const {
     getHomeSvg,
     getCategoriesSvg,
@@ -19,6 +31,25 @@ const Footer = ({ open }) => {
   const cartPage = checkLocation('cart');
   const wishlistPage = checkLocation('wishlist');
   const profilePage = checkLocation('profile');
+
+  useEffect(() => {
+    (async () => {
+      const value = await mounted(loaderData.accountData);
+      setAccountData(value);
+    })();
+  });
+
+  useEffect(() => {
+    if (accountData?.cart) {
+      setEmptyProduct((prevState) => ({ ...prevState, cartIsEmpty: false }));
+    }
+    if (accountData?.favorite) {
+      setEmptyProduct((prevState) => ({
+        ...prevState,
+        wishlistIsEmpty: false,
+      }));
+    }
+  }, [accountData]);
 
   return (
     <footer className="footer p-3">
@@ -49,23 +80,35 @@ const Footer = ({ open }) => {
           <NavLink
             to={`/${originPath}/cart`}
             className={({ isActive }) =>
-              `footer__link d-flex flex-column align-items-center ${
+              `footer__link d-flex position-relative flex-column align-items-center ${
                 isActive ? 'footer__link--active' : ''
               }`
             }
           >
             {getCartSvg('#6F7384', cartPage)}
+            {!emptyProduct.cartIsEmpty && (
+              <Badge
+                bg="danger"
+                className="stack__badge position-absolute end-0 d-inline rounded-5"
+              ></Badge>
+            )}
             <p className="m-0">My Cart</p>
           </NavLink>
           <NavLink
             to={`/${originPath}/wishlist`}
             className={({ isActive }) =>
-              `footer__link d-flex flex-column align-items-center ${
+              `footer__link d-flex position-relative flex-column align-items-center ${
                 isActive ? 'footer__link--active' : ''
               }`
             }
           >
             {getWishlistSvg('#6F7384', wishlistPage)}
+            {!emptyProduct.wishlistIsEmpty && (
+              <Badge
+                bg="danger"
+                className="stack__badge position-absolute end-0 d-inline rounded-5"
+              ></Badge>
+            )}
             <p className="m-0">Wishlist</p>
           </NavLink>
           <NavLink
